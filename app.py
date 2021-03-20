9d5fdca3480911305ca55c8a809b96fefc619834
lkarjun <lk.arjun@hotmail.com>
2021-03-20 12:21:10 +0530

    Updated

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
 import uvicorn
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory='templates')
 
 @app.get("/")
-def home():
-    return {"Hello": "Lalkrishan"}
+async def home(request: Request):
+    return templates.TemplateResponse('index.html', context={'request': request})
 
 
+@app.post("/files/")
+async def create_file(file: bytes = File(...)):
+    return {"file_size": len(file)}
 
-if __name__ == "__main__":
-    uvicorn.run(app)
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    return {"filename": file.filename}
