43507d8b8d7bb81b881d99900a73cafd47cf369b
lkarjun <lk.arjun@hotmail.com>
2021-03-22 16:19:59 +0530

    Out-img fixed

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,6 @@
 var loadFile = function(event){
     var image = document.getElementById('output')
     document.getElementById("choose").style="display: none;"
+    document.getElementById("output").style="display: initial"
     image.src = URL.createObjectURL(event.target.files[0]);
 }
