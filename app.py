88daa6191756ef629bee71c413561a0ee74c94d4
Lal Krishna Arjun <58617251+lkarjun@users.noreply.github.com>
2021-03-22 12:48:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from pickle import load
+# from pickle import load
 from fastapi import FastAPI, File, UploadFile
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
-from fastai.vision.all import load_learner
+# from fastai.vision.all import load_learner
 import uvicorn
 
 app = FastAPI()
@@ -20,7 +20,7 @@ async def home(request: Request):
 async def create_file(file: bytes = File(...)):
     return {"file_size": len(file)}
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
-    model = load_learner("Model\leaf-diseases-classifier.pkl")
-    return {"predicted": model.predict(file.file)}
+# @app.post("/uploadfile/")
+# async def create_upload_file(file: UploadFile = File(...)):
+#     model = load_learner("Model\leaf-diseases-classifier.pkl")
+#     return {"predicted": model.predict(file.file)}
