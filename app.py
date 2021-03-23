15f4dac55d976e90c2a7d2713b56eff4303fe2ba
lkarjun <lk.arjun@hotmail.com>
2021-03-23 09:36:55 +0530

    download_mode, predict function defined

--- app.py
+++ app.py
@@ -4,13 +4,31 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
-# from fastai.vision.all import Path
-import uvicorn
+from os import path
+from fastai.vision import open_image, load_learner
+from urllib.request import urlretrieve
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory='templates')
+
+
+def download_model():
+    '''Model downloading from google drive.'''
+    if path.exists('export.pkl') == False:
+        url = 'https://drive.google.com/uc?id=10CP_4IkQLpcIEDvRjKvJU2BJx7wKMTDz&export=download'
+        urlretrieve(url,'export.pkl')
+
+download_model()
+
+def predict(filename: str):
+    '''classifying image.'''
+    model = load_learner("")
+    img = open_image(filename)
+    pred_class, pred_idx = model.predict(img)
+    return str(pred_class)
+
 
 @app.get("/")
 async def home(request: Request):
@@ -31,4 +49,5 @@ async def create_upload_file(file: UploadFile = File(...)):
         with open(f"static/images/{file.filename}", 'wb') as f:
             f.write(contents)
         
-    return {"File": file.filename}
+        prediction = predict(f'static/images/{file.filename}')
+    return {"File": file.filename, "predicted": prediction}
