c45a0362d2392ad7fdb1c0e44ff68a15ecf76506
lkarjun <lk.arjun@hotmail.com>
2021-03-24 10:04:14 +0530

    Updated

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,7 @@
 var loadFile = function(event){
     var image = document.getElementById('output')
     document.getElementById("choose").style="display: none;"
-    document.getElementById("output").style="display: initial"
+    document.getElementById("output").style="display: block"
+    document.getElementById("upload").style="display: block"
     image.src = URL.createObjectURL(event.target.files[0]);
 }
