ec112f5ff8cf5d9417d915747fae4bf91603b5ee
lkarjun <lk.arjun@hotmail.com>
2021-03-24 12:49:33 +0530

    Image preview fixed

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,9 @@
 var loadFile = function(event){
     var image = document.getElementById('output')
     document.getElementById("choose").style="display: none;"
+    document.getElementById("displayImagediv").style="display: inline"
     document.getElementById("output").style="display: block"
     document.getElementById("upload").style="display: block"
+    
     image.src = URL.createObjectURL(event.target.files[0]);
 }
