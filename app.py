5256cb2e92aa47517e1392b38e225d7f8895e44b
lkarjun <lk.arjun@hotmail.com>
2021-03-29 17:03:59 +0530

    grapes intro

--- app.py
+++ app.py
@@ -14,16 +14,18 @@ templates = Jinja2Templates(directory='templates')
 
 def download_model(plant: str):
     '''Model downloading from google drive.'''
-    models_plant = {'tomato': 'https://github.com/lkarjun/fastai-workouts/blob/master/models/tomato.pkl?raw=true'}
-    if path.exists('export.pkl') == False:
+    models_plant = {'tomato': 'https://github.com/lkarjun/fastai-workouts/blob/master/models/tomato.pkl?raw=true',
+                    'grapes': 'https://github.com/lkarjun/fastai-workouts/blob/master/models/grapes.pkl?raw=true'}
+
+    if path.exists(f'{plant}.pkl') == False:
         url = models_plant[plant]
-        urlretrieve(url,'export.pkl')
+        urlretrieve(url,f'{plant}.pkl')
 
 def predict(filename: str, plant: str):
     '''classifying image.'''
     download_model(plant)
     # model = load_learner(Path.cwd()/'../models/export.pkl')
-    model = load_learner("export.pkl")
+    model = load_learner(f"{plant}.pkl")
     img = PILImage.create(filename)
     pred_class, pred_idx, ful_tensor = model.predict(img)
     return str(pred_class)
