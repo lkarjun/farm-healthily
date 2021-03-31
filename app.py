8316ee9bc58a513db43b1c6b90a2abdc28004a0b
lkarjun <lk.arjun@hotmail.com>
2021-03-31 20:06:22 +0530

    Major change - add background task

--- app.py
+++ app.py
@@ -1,41 +1,25 @@
-from fastapi import FastAPI, File, UploadFile, Form
+from fastapi import FastAPI, File, UploadFile, Form, Request, BackgroundTasks
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from starlette.requests import Request
-from os import path, remove
-from fastai.vision.all import PILImage, load_learner, Path
-from urllib.request import urlretrieve
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory='templates')
 
+model = None
 
-def download_model(plant: str):
-    '''Model downloading from google drive.'''
-    models_plant = {'tomato': 'https://github.com/lkarjun/fastai-workouts/blob/master/models/tomato.pkl?raw=true',
-                    'grapes': 'https://github.com/lkarjun/fastai-workouts/blob/master/models/grapes.pkl?raw=true',
-                    'potato': 'https://github.com/lkarjun/fastai-workouts/blob/master/models/potato.pkl?raw=true',
-                    'strawberry': 'https://github.com/lkarjun/fastai-workouts/blob/master/models/Strawberry.pkl?raw=true'}
-
-    if path.exists(f'{plant}.pkl') == False:
-        url = models_plant[plant]
-        urlretrieve(url,f'{plant}.pkl')
-
-def predict(filename: str, plant: str):
-    '''classifying image.'''
-    download_model(plant)
-    # model = load_learner(Path.cwd()/'../models/export.pkl')
-    model = load_learner(f"{plant}.pkl")
-    img = PILImage.create(filename)
-    pred_class, pred_idx, ful_tensor = model.predict(img)
-    return str(pred_class)
-
+def model_load():
+    print("-------------importing module Loaded-------------")
+    global model
+    from model_load import LoadModel
+    model = LoadModel()
+    print("-------------Importing completed-------------")
 
 @app.get("/")
-async def home(request: Request):
+async def home(request: Request, bg_task: BackgroundTasks):
     '''home page'''
+    bg_task.add_task(model_load)
     return templates.TemplateResponse('index.html', context={'request': request})
 
 
@@ -51,7 +35,7 @@ async def create_upload_file(file: UploadFile = File(...), plant = Form(...)):
         with open(path, 'wb') as f:
             f.write(contents)
         
-        prediction = predict(path, plant)
+        prediction = model.predict(filename = path, plant = plant)
         print(prediction)
-        remove(path)
+        model.remove_it(path)
     return {"File": file.filename, "predicted": prediction}
---
+++ model_load.py
@@ -0,0 +1,32 @@
+from fastai.vision.all import PILImage, load_learner, Path
+from os import path, remove
+from urllib.request import urlretrieve
+
+
+class LoadModel:
+
+    def download_model(self, plant: str):
+        '''Model downloading from google drive.'''
+        models_plant = {'tomato': 'https://github.com/lkarjun/fastai-workouts/blob/master/models/tomato.pkl?raw=true',
+                    'grapes': 'https://github.com/lkarjun/fastai-workouts/blob/master/models/grapes.pkl?raw=true',
+                    'potato': 'https://github.com/lkarjun/fastai-workouts/blob/master/models/potato.pkl?raw=true',
+                    'strawberry': 'https://github.com/lkarjun/fastai-workouts/blob/master/models/Strawberry.pkl?raw=true'}
+
+        if path.exists(f'{plant}.pkl') == False:
+            url = models_plant[plant]
+            urlretrieve(url,f'{plant}.pkl')
+
+    def predict(self, filename: str, plant: str):
+        '''classifying image.'''
+        print("-------------downloading-------------")
+        self.download_model(plant)
+        print("-------------downloaded-------------")
+        # model = load_learner(Path.cwd()/'../models/export.pkl')
+        model = load_learner(f"{plant}.pkl")
+        img = PILImage.create(filename)
+        pred_class, pred_idx, ful_tensor = model.predict(img)
+        return str(pred_class)
+    
+    def remove_it(self, filename):
+        '''removing uploaded photo'''
+        remove(filename)
